325cd882b4e845741fde828ef494465074e9da18
EmSH <emilie.haritchabelat@hotmail.fr>
2026-04-02 16:04:43 +0200

    correction après ajout d'une base de donnees dockerisee

--- src/main/java/tp/bibliotheque/repository/RessourceRepository.java
+++ src/main/java/tp/bibliotheque/repository/RessourceRepository.java
@@ -13,11 +13,11 @@ public interface RessourceRepository extends JpaRepository<RessourceDocumentaire
 
     @Query("""
         select r from RessourceDocumentaire r
-        where (:titre is null or lower(r.titre) like lower(concat('%', :titre, '%')))
-          and (:auteur is null or lower(r.auteur) like lower(concat('%', :auteur, '%')))
+        where lower(r.titre) like lower(concat('%', coalesce(:titre, ''), '%'))
+          and lower(r.auteur) like lower(concat('%', coalesce(:auteur, ''), '%'))
           and (:annee is null or r.anneePublication = :annee)
-          and (:theme is null or lower(r.theme) like lower(concat('%', :theme, '%')))
-        """)
+          and lower(r.theme) like lower(concat('%', coalesce(:theme, ''), '%'))
+    """)
     List<RessourceDocumentaire> rechercheAvancee(
             @Param("titre") String titre,
             @Param("auteur") String auteur,
